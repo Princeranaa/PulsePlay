33eb2beb51d7480e3d4a6b0d10980911ac112028
Prince Rana <ranaprince45may@gmail.com>
2025-12-14 20:16:48 +0530

    add the cors

--- music/app.js
+++ music/app.js
@@ -2,11 +2,15 @@ import express from 'express'
 const app = express()
 import musicRouter from "./routes/music.routes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 
 
 
-
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 app.use(express.json());
 app.use(cookieParser());
